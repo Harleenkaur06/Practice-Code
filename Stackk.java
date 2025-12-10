b6b437129353f7fe282c0cb8034130e4852ab6f6
Harleenkaur06 <harleenkaur3180@gmail.com>
2025-12-10 22:47:27 +0530

    insert at bottom code

--- Stackk.java
+++ Stackk.java
@@ -189,36 +189,63 @@
 // }
 
 //reversing a stack recursively
-import java.util.*;
-public class Stackk{
-    public static void pushatbottom(int x,Stack<Integer>st){
-        Stack<Integer> temp=new Stack<>();
-        while(st.size()>0){
-            temp.push(st.pop());
-        }
-        st.push(x);
-        while(temp.size()>0){
-            st.push(temp.pop());
-        }
+// import java.util.*;
+// public class Stackk{
+//     public static void pushatbottom(int x,Stack<Integer>st){
+//         Stack<Integer> temp=new Stack<>();
+//         while(st.size()>0){
+//             temp.push(st.pop());
+//         }
+//         st.push(x);
+//         while(temp.size()>0){
+//             st.push(temp.pop());
+//         }
 
-    }
-    public static void reversestack(Stack<Integer> st){
-        if(st.size()==1) return ;
-        int top=st.pop();
-        reversestack(st);
-        pushatbottom(top,st);
+//     }
+//     public static void reversestack(Stack<Integer> st){
+//         if(st.size()==1) return ;
+//         int top=st.pop();
+//         reversestack(st);
+//         pushatbottom(top,st);
 
-    }
+//     }
    
+//     public static void main(String[] args) {
+//         Stack<Integer> st=new Stack<>();
+//         st.push(5);
+//         st.push(3);
+//         st.push(10);
+//         st.push(90);
+//         st.push(100);
+//         System.out.println(st);
+//         reversestack(st);
+//         System.out.println(st);
+//     }
+// }
+
+//insert at bottom;
+import  java.util.*;
+class Stackk{
     public static void main(String[] args) {
         Stack<Integer> st=new Stack<>();
+        Scanner sc=new Scanner(System.in);
         st.push(5);
-        st.push(3);
-        st.push(10);
+        st.push(23);
         st.push(90);
-        st.push(100);
+        st.push(80);
+       
         System.out.println(st);
-        reversestack(st);
+        Stack<Integer> temp=new Stack<>();
+        while(st.size()!=0){
+            temp.push(st.pop());
+        }
+        System.out.println(temp);
+        int n=sc.nextInt();
+        st.push(n);
+        
+        while(temp.size()!=0){
+            st.push(temp.pop());
+        }
         System.out.println(st);
     }
 }
