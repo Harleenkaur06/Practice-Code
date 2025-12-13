6722023b8124035bcfb187fa48dd769cb44793ad
Harleenkaur06 <harleenkaur3180@gmail.com>
2025-12-13 22:11:22 +0530

    infix to postfix expresion

--- Stackk.java
+++ Stackk.java
@@ -840,10 +840,70 @@
 
 
 //prefix expression
+// import java.util.*;
+// class Stackk{
+//     public static void main(String[] args) {
+//         String str="9-(5+3)*4/6";
+//         Stack<String> val=new Stack<>();
+//         Stack<Character> op=new Stack<>();
+//         for(int i=0;i<str.length();i++){
+//             char ch=str.charAt(i);
+//             int ascii=(int)ch;
+//             if(ascii>=48 && ascii<=57){
+//                 String s=""+ch;
+//                 val.push(s);
+//             }else if(op.size()==0 || ch=='('|| op.peek()=='('){
+//                 op.push(ch);
+//             }else if(ch==')'){
+//                 while(op.peek()!='('){
+//                     String v2=val.pop();
+//                     String v1=val.pop();
+//                     char o=op.pop();
+//                     String x=o+v1+v2;
+//                     val.push(x);
+//                 }
+//                 op.pop();
+//             }else{
+//                 if(ch=='+' || ch=='-'){
+//                     String v2=val.pop();
+//                     String v1=val.pop();
+//                     char o=op.pop();
+//                     String x=o+v1+v2;
+//                     val.push(x);
+//                     op.push(ch);
+//                 }else if(ch=='*' || ch=='/'){
+//                     if(op.peek()=='*' || op.peek()=='/'){
+//                         String v2=val.pop();
+//                     String v1=val.pop();
+//                     char o=op.pop();
+//                     String x=o+v1+v2;
+//                     val.push(x);
+//                     op.push(ch);
+//                     }
+//                     else {op.push(ch);}
+//                 }
+//             }
+            
+//         }
+//         while(val.size()>1){
+//             String v2=val.pop();
+//                     String v1=val.pop();
+//                     char o=op.pop();
+//                     String x=o+v1+v2;
+//                     val.push(x);
+                    
+//         }
+//         System.out.println(val.peek());
+
+//     }
+// }
+
+
+//post prefix
 import java.util.*;
 class Stackk{
     public static void main(String[] args) {
-        String str="9-(5+3)*4/6";
+        String str="9-(5+3)*4/6";//953+4*6/-
         Stack<String> val=new Stack<>();
         Stack<Character> op=new Stack<>();
         for(int i=0;i<str.length();i++){
@@ -859,7 +919,7 @@ class Stackk{
                     String v2=val.pop();
                     String v1=val.pop();
                     char o=op.pop();
-                    String x=o+v1+v2;
+                    String x=v1+v2+o;
                     val.push(x);
                 }
                 op.pop();
@@ -868,7 +928,7 @@ class Stackk{
                     String v2=val.pop();
                     String v1=val.pop();
                     char o=op.pop();
-                    String x=o+v1+v2;
+                    String x=v1+v2+o;
                     val.push(x);
                     op.push(ch);
                 }else if(ch=='*' || ch=='/'){
@@ -876,7 +936,7 @@ class Stackk{
                         String v2=val.pop();
                     String v1=val.pop();
                     char o=op.pop();
-                    String x=o+v1+v2;
+                    String x=v1+v2+o;
                     val.push(x);
                     op.push(ch);
                     }
@@ -889,7 +949,7 @@ class Stackk{
             String v2=val.pop();
                     String v1=val.pop();
                     char o=op.pop();
-                    String x=o+v1+v2;
+                    String x=v1+v2+o;
                     val.push(x);
                     
         }
