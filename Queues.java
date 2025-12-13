bdd54d817189ad2005e4abacedbafd86995e93d1
Harleenkaur06 <harleenkaur3180@gmail.com>
2025-12-14 00:03:00 +0530

    implementing circular queue using array

--- Queues.java
+++ Queues.java
@@ -93,56 +93,131 @@
 // }
 
 //implementing queue using linkedlist
- class Queues{
-    public static class Node{
-        int data;
-        Node next;
-        Node(int data){
-            this.data=data;
-        }
-    }
-    public static class LQueue{
-        Node head=null;
-        Node tail=null;
+//  class Queues{
+//     public static class Node{
+//         int data;
+//         Node next;
+//         Node(int data){
+//             this.data=data;
+//         }
+//     }
+//     public static class LQueue{
+//         Node head=null;
+//         Node tail=null;
+//         int s=0;
+//         void add(int val){
+//             Node temp=new Node(val);
+//             if(head==null){
+//                 head=tail=temp;
+//             }else{
+//                 tail.next=temp;
+//                 tail=temp;
+//             }
+//             s++;
+//         }
+//         int peek(){
+//             if(s==0) return -1;
+//             return head.data;
+//         }
+//         int remove(){
+//             if(s==0) return -1;
+//             int top=head.data;
+//             head=head.next;
+//             s--;
+//             return top;
+//         }
+//         void display(){
+//             if(head==null){
+//                 System.out.println("queue is empty");
+//                 return;
+//             }
+//             Node temp=head;
+//             while(temp!=null){
+//                 System.out.print(temp.data+" ");
+//                 temp=temp.next;
+//             }
+//             System.out.println();
+//         }
+
+//     }
+//     public static void main(String[] args) {
+//         LQueue q=new LQueue();
+//         q.display();
+//         q.add(1);
+//         q.add(2);
+//         q.add(3);
+//         q.add(4);
+//         q.add(5);
+//         q.display();
+//         q.remove();
+//         q.remove();
+//         q.display();
+//         System.out.println(q.peek());
+//     }
+//  }
+
+ //array implementation of circular queue
+ class  Queues{
+    public static class Aqueue{
+        int f=-1;
+        int r=-1;
         int s=0;
+        int[] arr=new int[10];
         void add(int val){
-            Node temp=new Node(val);
-            if(head==null){
-                head=tail=temp;
-            }else{
-                tail.next=temp;
-                tail=temp;
+            if (s == arr.length) {
+                System.out.println("queue is full");
+                return;
+            }
+            if(s==0){
+                f=r=0;
+                arr[0]=val;
+                
+            }
+            else if(r<=arr.length-1){
+                arr[++r]=val;
+            }else if(r==arr.length-1){
+                r=0;
+                arr[r]=val; 
             }
             s++;
         }
-        int peek(){
-            if(s==0) return -1;
-            return head.data;
-        }
-        int remove(){
-            if(s==0) return -1;
-            int top=head.data;
-            head=head.next;
-            s--;
-            return top;
+        int remove() throws Exception{
+            if(s==0) {
+                throw new Exception("queue is empty");
+            }
+            
+            else {
+                int t=arr[f];
+                if(f==arr.length-1) f=0;
+                
+                else f++;
+                s--;
+                return t;
+            }
         }
         void display(){
-            if(head==null){
+            if(s==0){
                 System.out.println("queue is empty");
                 return;
-            }
-            Node temp=head;
-            while(temp!=null){
-                System.out.print(temp.data+" ");
-                temp=temp.next;
+            }else if(f<=r){
+                for(int i=f;i<=r;i++){
+                    System.out.print(arr[i]+" ");
+                }
+            }else{
+                for(int i=f;i<=arr.length-1;i++){
+                    System.out.print(arr[i]+" ");
+                }
+                for(int i=0;i<=r;i++){
+                    System.out.print(arr[i]+" ");
+                }
             }
             System.out.println();
         }
 
     }
-    public static void main(String[] args) {
-        LQueue q=new LQueue();
-        q.display();
+    public static void main(String[] args) throws Exception{
+        Aqueue q=new Aqueue();
+        //q.remove();
         q.add(1);
         q.add(2);
         q.add(3);
@@ -150,10 +225,11 @@
         q.add(5);
         q.display();
         q.remove();
-        q.remove();
+         q.remove();
         q.display();
-        System.out.println(q.peek());
-
         
+
+
     }
+
  }
