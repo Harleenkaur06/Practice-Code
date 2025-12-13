9c4717faf90c00f0506360bfcc15fa5ac788171b
Harleenkaur06 <harleenkaur3180@gmail.com>
2025-12-13 22:25:35 +0530

    to view element at top and size in queue

--- Queues.java
+++ Queues.java
@@ -10,6 +10,8 @@ public class Queues {
         System.out.println(q);//1 2 3 4 5
         q.remove();
         System.out.println(q);//2 3 4 5
+        System.out.println(q.element());
+        System.out.println(q.size());
     }
     
 }
