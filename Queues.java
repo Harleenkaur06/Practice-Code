8a184ab0ec1a3c25bf816ec939d3ec1b78b5e8ae
Harleenkaur06 <harleenkaur3180@gmail.com>
2025-12-13 22:33:22 +0530

    displaying queue using extra queue

--- Queues.java
+++ Queues.java
@@ -1,3 +1,23 @@
+// import java.util.*;
+// public class Queues {
+//     public static void main(String[] args) {
+//         Queue<Integer> q=new LinkedList<>();
+//         q.add(1);
+//         q.add(2);
+//         q.add(3);
+//         q.add(4);
+//         q.add(5);
+//         System.out.println(q);//1 2 3 4 5
+//         q.remove();
+//         System.out.println(q);//2 3 4 5
+//         System.out.println(q.element());
+//         System.out.println(q.size());
+//     }
+    
+// }
+
+
+//print all the elemet using queue
 import java.util.*;
 public class Queues {
     public static void main(String[] args) {
@@ -7,11 +27,15 @@ public class Queues {
         q.add(3);
         q.add(4);
         q.add(5);
-        System.out.println(q);//1 2 3 4 5
-        q.remove();
-        System.out.println(q);//2 3 4 5
-        System.out.println(q.element());
-        System.out.println(q.size());
+        Queue<Integer> temp=new LinkedList<>();
+        while(q.size()>0){
+            System.out.print(q.element()+" ");
+            temp.add(q.remove());
+        }
+        while(temp.size()>0){
+            
+            q.add(temp.poll());
+        }
     }
     
 }
