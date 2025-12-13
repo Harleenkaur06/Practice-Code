ba7239a8c6ab56af588a8faaa930761b9d7182c0
Harleenkaur06 <harleenkaur3180@gmail.com>
2025-12-13 15:05:30 +0530

    stock span

--- Stackk.java
+++ Stackk.java
@@ -625,21 +625,52 @@
 
 
 //previous greater
+// import java.util.*;
+// class Stackk{
+//     public static int[] greater(int[] arr){
+//         int n=arr.length;
+//         int[] res=new int[n];
+//         Stack<Integer> st=new Stack<>();
+//         res[0]=-1;
+//         st.push(arr[0]);
+//         for(int i=1;i<n;i++){
+//             while(st.size()>0 && st.peek()<arr[i]){
+//                 st.pop();
+//             }
+//             if(st.size()==0) res[i]=-1;
+//             else{res[i]=st.peek();}
+//             st.push(arr[i]);
+//         }
+//         return res;
+
+//     }
+//     public static void main(String[] args) {
+//         int[] arr={100,80,60,74,85};
+//         int[] res=greater(arr);
+//         for(int i=0;i<res.length;i++){
+//             System.out.print(res[i]+" ");
+//         }
+//     }
+// }
+
+
+//stock span
 import java.util.*;
 class Stackk{
     public static int[] greater(int[] arr){
         int n=arr.length;
         int[] res=new int[n];
         Stack<Integer> st=new Stack<>();
-        res[0]=-1;
-        st.push(arr[0]);
+        res[0]=1;
+        st.push(0);
         for(int i=1;i<n;i++){
-            while(st.size()>0 && st.peek()<arr[i]){
+            
+            while(st.size()>0 && arr[st.peek()]<arr[i]){
                 st.pop();
             }
-            if(st.size()==0) res[i]=-1;
-            else{res[i]=st.peek();}
-            st.push(arr[i]);
+            if(st.size()==0) res[i]=1;
+            else{res[i]=i-st.peek();}
+            st.push(i);
         }
         return res;
 
@@ -652,4 +683,3 @@ class Stackk{
         }
     }
 }
-
