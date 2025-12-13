10a46be6738b19587e7ba43ff00004255d4248cd
Harleenkaur06 <harleenkaur3180@gmail.com>
2025-12-13 22:57:55 +0530

    implementing queue using array

--- Queues.java
+++ Queues.java
@@ -1,4 +1,4 @@
-// import java.util.*;
+
 // public class Queues {
 //     public static void main(String[] args) {
 //         Queue<Integer> q=new LinkedList<>();
@@ -18,23 +18,77 @@
 
 
 //print all the elemet using queue
-import java.util.*;
-public class Queues {
-    public static void main(String[] args) {
-        Queue<Integer> q=new LinkedList<>();
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        q.add(4);
-        q.add(5);
-        Queue<Integer> temp=new LinkedList<>();
-        while(q.size()>0){
-            System.out.print(q.peek()+" ");
-            temp.add(q.remove());
-        }
-        while(temp.size()>0){
-            q.add(temp.poll());
-        }
-    }
+// import java.util.*;
+// public class Queues {
+//     public static void main(String[] args) {
+//         Queue<Integer> q=new LinkedList<>();
+//         q.add(1);
+//         q.add(2);
+//         q.add(3);
+//         q.add(4);
+//         q.add(5);
+//         Queue<Integer> temp=new LinkedList<>();
+//         while(q.size()>0){
+//             System.out.print(q.peek()+" ");
+//             temp.add(q.remove());
+//         }
+//         while(temp.size()>0){
+//             q.add(temp.poll());
+//         }
+//     }
     
+// }
+
+//array implementation of queue
+class Queues{
+    public static class AQueue{
+        int[] arr=new int[10];
+        int f=-1;
+        int r=-1;
+        int s=0;
+        void add(int val){
+            if(f==-1){
+                f=0;
+                arr[++r]=val;
+            }else{
+                arr[++r]=val;
+            }
+            s++;
+        }
+        int remove(){
+            if(s==0) return -1;
+            f++;
+            s--;
+            return arr[f-1];
+        }
+        int peek(){
+            if(s==0) return -1;
+            return arr[f];
+        }
+        void display(){
+            if(s==0){
+                System.out.println("queue is empty");
+            }
+            for(int i=f;i<=r;i++){
+                System.out.print(arr[i]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        AQueue q=new AQueue();
+        q.add(5);
+        q.add(3);
+        q.add(1);
+        q.add(8);
+        q.add(9);
+        q.display();
+        q.remove();
+        q.remove();
+        q.display();
+        System.out.println(q.peek());
+
+    }
 }
+
