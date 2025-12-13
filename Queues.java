9e1246a35f0c726e55d27bfc61a0a701a1a12d25
Harleenkaur06 <harleenkaur3180@gmail.com>
2025-12-13 22:24:07 +0530

    implementing built in queue using linkedlist

---
+++ Queues.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Queues {
+    public static void main(String[] args) {
+        Queue<Integer> q=new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        System.out.println(q);//1 2 3 4 5
+        q.remove();
+        System.out.println(q);//2 3 4 5
+    }
+    
+}
--- Stackk.java
+++ Stackk.java
@@ -900,60 +900,60 @@
 
 
 //post prefix
-import java.util.*;
-class Stackk{
-    public static void main(String[] args) {
-        String str="9-(5+3)*4/6";//953+4*6/-
-        Stack<String> val=new Stack<>();
-        Stack<Character> op=new Stack<>();
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            int ascii=(int)ch;
-            if(ascii>=48 && ascii<=57){
-                String s=""+ch;
-                val.push(s);
-            }else if(op.size()==0 || ch=='('|| op.peek()=='('){
-                op.push(ch);
-            }else if(ch==')'){
-                while(op.peek()!='('){
-                    String v2=val.pop();
-                    String v1=val.pop();
-                    char o=op.pop();
-                    String x=v1+v2+o;
-                    val.push(x);
-                }
-                op.pop();
-            }else{
-                if(ch=='+' || ch=='-'){
-                    String v2=val.pop();
-                    String v1=val.pop();
-                    char o=op.pop();
-                    String x=v1+v2+o;
-                    val.push(x);
-                    op.push(ch);
-                }else if(ch=='*' || ch=='/'){
-                    if(op.peek()=='*' || op.peek()=='/'){
-                        String v2=val.pop();
-                    String v1=val.pop();
-                    char o=op.pop();
-                    String x=v1+v2+o;
-                    val.push(x);
-                    op.push(ch);
-                    }
-                    else {op.push(ch);}
-                }
-            }
+// import java.util.*;
+// class Stackk{
+//     public static void main(String[] args) {
+//         String str="9-(5+3)*4/6";//953+4*6/-
+//         Stack<String> val=new Stack<>();
+//         Stack<Character> op=new Stack<>();
+//         for(int i=0;i<str.length();i++){
+//             char ch=str.charAt(i);
+//             int ascii=(int)ch;
+//             if(ascii>=48 && ascii<=57){
+//                 String s=""+ch;
+//                 val.push(s);
+//             }else if(op.size()==0 || ch=='('|| op.peek()=='('){
+//                 op.push(ch);
+//             }else if(ch==')'){
+//                 while(op.peek()!='('){
+//                     String v2=val.pop();
+//                     String v1=val.pop();
+//                     char o=op.pop();
+//                     String x=v1+v2+o;
+//                     val.push(x);
+//                 }
+//                 op.pop();
+//             }else{
+//                 if(ch=='+' || ch=='-'){
+//                     String v2=val.pop();
+//                     String v1=val.pop();
+//                     char o=op.pop();
+//                     String x=v1+v2+o;
+//                     val.push(x);
+//                     op.push(ch);
+//                 }else if(ch=='*' || ch=='/'){
+//                     if(op.peek()=='*' || op.peek()=='/'){
+//                         String v2=val.pop();
+//                     String v1=val.pop();
+//                     char o=op.pop();
+//                     String x=v1+v2+o;
+//                     val.push(x);
+//                     op.push(ch);
+//                     }
+//                     else {op.push(ch);}
+//                 }
+//             }
             
-        }
-        while(val.size()>1){
-            String v2=val.pop();
-                    String v1=val.pop();
-                    char o=op.pop();
-                    String x=v1+v2+o;
-                    val.push(x);
+//         }
+//         while(val.size()>1){
+//             String v2=val.pop();
+//                     String v1=val.pop();
+//                     char o=op.pop();
+//                     String x=v1+v2+o;
+//                     val.push(x);
                     
-        }
-        System.out.println(val.peek());
+//         }
+//         System.out.println(val.peek());
 
-    }
-}
+//     }
+// }
