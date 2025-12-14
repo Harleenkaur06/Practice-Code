83d8f02310cbf61ad5cdd6c9dd5458429f42f14a
Harleenkaur06 <harleenkaur3180@gmail.com>
2025-12-14 14:49:50 +0530

    basic string

---
+++ Strings.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Strings {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str="hello world!";
+        String str2=sc.nextLine();
+        System.out.println(str);
+        System.out.println(str2);
+        System.out.println(str.length());
+        System.out.println(str2.length());
+        System.out.println(str.charAt(8));
+        System.out.println(str2.charAt(7));
+        
+
+    }
+    
+}
