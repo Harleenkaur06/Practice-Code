932962de641642d6dd2aa3646c2db688e00d5906
Harleenkaur06 <harleenkaur3180@gmail.com>
2025-12-14 15:27:22 +0530

    toggle uppercase to lowercase using stringbuilder

--- Strings.java
+++ Strings.java
@@ -13,20 +13,67 @@
 //     }
 // }
 
+//stringbuilder and basic function
+// import java.util.*;
+// public class Strings {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         StringBuilder str=new StringBuilder("hello");
+//         str.append("world");
+//         System.out.println(str);
+//         str.setCharAt(0,'m');//melloworld
+//         System.out.println(str);
+//         str.insert(2, 'y');
+//         System.out.println(str);//meylloworld
+//         str.deleteCharAt(2);
+//         System.out.println(str);//melloworld
+//         str.delete(0, 2);//2 not included
+//         System.out.println(str);//lloworld
+//     }
+// }
+
+//reverse
+// import java.util.*;
+// public class Strings {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         StringBuilder str=new StringBuilder("hello");
+//         // str.reverse();
+//         // System.out.println(str);
+
+//     }
+// }
+
+//the letter in upper to lower and lower to upper
+//with stringbuilder
 import java.util.*;
 public class Strings {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        StringBuilder str=new StringBuilder("hello");
-        str.append("world");
+        StringBuilder str=new StringBuilder(sc.nextLine());
+        //toggle
+        //pHYsics;
+        for(int i=0;i<str.length();i++){
+            int f=0;
+            char ch=str.charAt(i);
+            int ascii=(int)ch;
+            if(ch==' '){
+                continue;
+            }
+            if(ascii>=97){
+                f=1;//small letter
+            }
+            if(f==0){
+                ascii+=32;
+                char dh=(char)ascii;
+                str.setCharAt(i, dh);
+            }else{
+                ascii-=32;
+                char sh=(char)ascii;
+                str.setCharAt(i, sh);
+            }
+        }
+        
         System.out.println(str);
-        str.setCharAt(0,'m');//melloworld
-        System.out.println(str);
-        str.insert(2, 'y');
-        System.out.println(str);//meylloworld
-        str.deleteCharAt(2);
-        System.out.println(str);//melloworld
-        str.delete(0, 2);//2 not included
-        System.out.println(str);//lloworld
     }
 }
