a2ba56d77b25118d25d2dfe9836b61dd800859d5
Harleenkaur06 <harleenkaur3180@gmail.com>
2026-01-01 22:20:54 +0530

    printing numbers in reverse order using recursion

--- Recursion.java
+++ Recursion.java
@@ -1,12 +1,30 @@
-//a prog to print all natural no. from 1 to n
+// a prog to print all natural no. from 1 to n
+// import java.util.*;
+// public class Recursion {
+//     public static void print(int n){
+//         if(n==1){
+//             System.out.println(1);
+//             return;}
+//         print(n-1);
+//         System.out.println(n);
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         print(n);
+//     }
+// }
+
+//a prog to print all natural no. from n to 1
 import java.util.*;
 public class Recursion {
     public static void print(int n){
         if(n==1){
             System.out.println(1);
             return;}
-        print(n-1);
         System.out.println(n);
+        print(n-1);
+        
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
