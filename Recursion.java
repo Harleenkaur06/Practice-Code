56b9119b3280d7414f5326517ea3e4755d910df8
Harleenkaur06 <harleenkaur3180@gmail.com>
2026-01-02 19:24:07 +0530

    bubble sort

--- Recursion.java
+++ Recursion.java
@@ -16,19 +16,19 @@
 // }
 
 //a prog to print all natural no. from n to 1
-import java.util.*;
-public class Recursion {
-    public static void print(int n){
-        if(n==1){
-            System.out.println(1);
-            return;}
-        System.out.println(n);
-        print(n-1);
+// import java.util.*;
+// public class Recursion {
+//     public static void print(int n){
+//         if(n==1){
+//             System.out.println(1);
+//             return;}
+//         System.out.println(n);
+//         print(n-1);
         
-    }
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        print(n);
-    }
-}
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         print(n);
+//     }
+// }
