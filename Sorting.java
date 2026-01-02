cb039f7eb1242298749e1eac735a8dcf082d6cc9
Harleenkaur06 <harleenkaur3180@gmail.com>
2026-01-02 19:27:48 +0530

    optimized bubble sort

--- Sorting.java
+++ Sorting.java
@@ -1,4 +1,29 @@
 //bubble sort
+// import java.util.*;
+// public class Sorting {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int[] arr=new int[n];
+//         for(int i=0;i<n;i++){
+//             arr[i]=sc.nextInt();
+//         }
+//         for(int i=0;i<n-1;i++){
+//             for(int j=0;j<n-i-1;j++){
+//                 if(arr[j]>arr[j+1]){
+//                     int t=arr[j];
+//                     arr[j]=arr[j+1];
+//                     arr[j+1]=t;
+//                 }
+//             }
+//         }
+//         for(int i=0;i<n;i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+// }
+
+//optimized bubble sort
 import java.util.*;
 public class Sorting {
     public static void main(String[] args) {
@@ -9,12 +34,17 @@ public class Sorting {
             arr[i]=sc.nextInt();
         }
         for(int i=0;i<n-1;i++){
+            boolean swap=false;
             for(int j=0;j<n-i-1;j++){
                 if(arr[j]>arr[j+1]){
                     int t=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=t;
+                    swap=true;
                 }
+            }
+            if(!swap){
+                break;
             }
         }
         for(int i=0;i<n;i++){
