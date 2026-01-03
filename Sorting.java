728f47ffc4d3d9c32af9af1e42c9dfa9ed05d89a
Harleenkaur06 <harleenkaur3180@gmail.com>
2026-01-03 18:44:21 +0530

    insertion sort

--- Sorting.java
+++ Sorting.java
@@ -55,6 +55,33 @@
 
 //selection sort
 //select a element and put it in correct position
+// import java.util.*;
+// public class Sorting {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int[] arr=new int[n];
+//         for(int i=0;i<n;i++){
+//             arr[i]=sc.nextInt();
+//         }
+//         for(int i=0;i<n;i++){
+//             int min=i;
+//             for(int j=i;j<n;j++){
+//                 if(arr[j]<arr[min]){
+//                     min=j;
+//                 }
+//             }
+//             int t=arr[i];
+//             arr[i]=arr[min];
+//             arr[min]=t;
+//         }
+//         for(int i=0;i<n;i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+// }
+
+//insertion sort
 import java.util.*;
 public class Sorting {
     public static void main(String[] args) {
@@ -65,15 +92,13 @@ public class Sorting {
             arr[i]=sc.nextInt();
         }
         for(int i=0;i<n;i++){
-            int min=i;
-            for(int j=i;j<n;j++){
-                if(arr[j]<arr[min]){
-                    min=j;
-                }
+            int j=i;
+            while(j>0 && arr[j-1]>arr[j]){
+                int t=arr[j-1];
+                arr[j-1]=arr[j];
+                arr[j]=t;
+                j--;
             }
-            int t=arr[i];
-            arr[i]=arr[min];
-            arr[min]=t;
         }
         for(int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
