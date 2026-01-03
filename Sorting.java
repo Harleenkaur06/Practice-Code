164869fbf554785bb33ab02dce205a298b2c448b
Harleenkaur06 <harleenkaur3180@gmail.com>
2026-01-03 18:39:53 +0530

    selection sort

--- Sorting.java
+++ Sorting.java
@@ -24,6 +24,37 @@
 // }
 
 //optimized bubble sort
+// import java.util.*;
+// public class Sorting {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int[] arr=new int[n];
+//         for(int i=0;i<n;i++){
+//             arr[i]=sc.nextInt();
+//         }
+//         for(int i=0;i<n-1;i++){
+//             boolean swap=false;
+//             for(int j=0;j<n-i-1;j++){
+//                 if(arr[j]>arr[j+1]){
+//                     int t=arr[j];
+//                     arr[j]=arr[j+1];
+//                     arr[j+1]=t;
+//                     swap=true;
+//                 }
+//             }
+//             if(!swap){
+//                 break;
+//             }
+//         }
+//         for(int i=0;i<n;i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+// }
+
+//selection sort
+//select a element and put it in correct position
 import java.util.*;
 public class Sorting {
     public static void main(String[] args) {
@@ -33,19 +64,16 @@ public class Sorting {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        for(int i=0;i<n-1;i++){
-            boolean swap=false;
-            for(int j=0;j<n-i-1;j++){
-                if(arr[j]>arr[j+1]){
-                    int t=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=t;
-                    swap=true;
+        for(int i=0;i<n;i++){
+            int min=i;
+            for(int j=i;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
                 }
             }
-            if(!swap){
-                break;
-            }
+            int t=arr[i];
+            arr[i]=arr[min];
+            arr[min]=t;
         }
         for(int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
