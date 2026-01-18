b67881fd14eaf7b01b8ff897293c57db85a1ca65
Harleenkaur06 <harleenkaur3180@gmail.com>
2026-01-18 21:46:07 +0530

    making doubly linkedlist and addatlast function

--- DoublyLL.java
+++ DoublyLL.java
@@ -1,3 +1,56 @@
+// public class DoublyLL {
+//     public static class Node{
+//         int data;
+//         Node next;
+//         Node prev;
+//         Node(int data){
+//             this.data=data;
+//         }
+//     }
+//     public static void display(Node head){
+//         Node t=head;
+//         while(t!=null){
+//             System.out.print(t.data+" ");
+//             t=t.next;
+//         }
+//         System.out.println();
+//     }
+//     public static void displayrev(Node tail){
+//         Node t=tail;
+//         while(t!=null){
+//             System.out.print(t.data+" ");
+//             t=t.prev;
+//         }
+//         System.out.println();
+//     }
+//     public static void displayrandom(Node random){
+//         Node t=random;
+//         while(t.prev!=null){
+//             t=t.prev;
+//         }
+//         display(t);
+//     }
+//     public static void main(String[] args) {
+//         Node n1=new Node(4);
+//         Node n2=new Node(3);
+//         Node n3=new Node(1);
+//         Node n4=new Node(20);
+//         Node n5=new Node(40);
+//         n1.next=n2;
+//         n2.prev=n1;
+//         n2.next=n3;
+//         n3.prev=n2;
+//         n3.next=n4;
+//         n4.prev=n3;
+//         n4.next=n5;
+//         n5.prev=n4;
+//         display(n1);
+//         displayrev(n5);
+//         displayrandom(n3);
+//     }
+// }
+
+//inserting in dll
 public class DoublyLL {
     public static class Node{
         int data;
@@ -7,35 +60,37 @@ public class DoublyLL {
             this.data=data;
         }
     }
-    public static void display(Node head){
-        Node t=head;
-        while(t!=null){
-            System.out.println(t.data);
-            t=t.next;
+    public static class doubly{
+        Node head=null;
+        Node tail=null;
+        void addatlast(int val){
+            Node newnode=new Node(val);
+            if(head==null){
+                head=tail=newnode;
+            }else{
+                newnode.prev=tail;
+                tail.next=newnode;
+                tail=newnode;
+            }
         }
-    }
-    public static void display2(Node tail){
-        Node t=tail;
-        while(t!=null){
-            System.out.println(t.data);
-            t=t.prev;
+    
+        
+        void display(){
+            Node t=head;
+            while(t!=null){
+                System.out.print(t.data+" ");
+                t=t.next;
+            }
+            System.out.println();
         }
+        
     }
     public static void main(String[] args) {
-        Node n1=new Node(4);
-        Node n2=new Node(3);
-        Node n3=new Node(1);
-        Node n4=new Node(20);
-        Node n5=new Node(40);
-        n1.next=n2;
-        n2.prev=n1;
-        n2.next=n3;
-        n3.prev=n2;
-        n3.next=n4;
-        n4.prev=n3;
-        n4.next=n5;
-        n5.prev=n4;
-        display(n1);
-        display2(n5);
+        doubly ll=new doubly();
+        ll.addatlast(4);
+        ll.addatlast(3);
+        ll.addatlast(2);
+        ll.addatlast(1);
+        ll.display();;
     }
 }
